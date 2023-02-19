61e32ec055b9cdf55a461b3ef2c2d1bb2e217a9e
Emir Şener <emir@cbot.ai>
2023-02-19 23:25:38 +0300

    send sms

--- index.js
+++ index.js
@@ -2,28 +2,47 @@ const express = require(`express`);
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const fs = require('fs');
+const path = require('path');
 
 
+const uiPath = path.join( __dirname, 'panel', 'ae-emergence-frontend');
 const corsOptions = {
     origin: '*',
     credentials: true,
     optionSuccessStatus: 200,
 }
 
+
 // PRE PROCESS.
 app.use(cors(corsOptions))
 app.use(express.json());
-app.use(require('./middleware/logger'));
+app.use('/api', require('./middleware/logger'));
 // app.use(require('./middleware/secure'));
 
 // API'S
-app.use(require('./routes/auth'));
-app.use(require('./routes/employee'));
-app.use(require('./routes/upload'));
-app.use(require('./routes/sms'));
-app.use(require('./routes/stats'));
-//POST PROCESS
-app.use(require('./middleware/error'));
+app.use('/api', require('./routes/auth'));
+app.use('/api', require('./routes/employee'));
+app.use('/api', require('./routes/upload'));
+app.use('/api', require('./routes/sms'));
+app.use('/api', require('./routes/stats'));
+// POST PROCESS
+app.use('/api', require('./middleware/error'));
+// FRONT-END
+app.use(express.static(uiPath));
+app.get('*', function (req, res, next) {
+    try {
+        fs.stat(uiPath + req.path, function (err) {
+            if (err) {
+                res.sendFile(uiPath + "/index.html", { uiPath });
+            } else {
+                res.sendFile(req.path, { uiPath });
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 
 const serve = async () => {
--- routes/employee.js
+++ routes/employee.js
@@ -3,7 +3,33 @@ const employeesModel = require('../models/employee.model');
 
 router.get('/employees', async (req, res, next) => {
     try {
-        let employees = await employeesModel.find({});
+        let filter = req.query.filter;
+        var employees;
+        if (filter != null && filter != undefined && filter != 'undefined') {
+            filter = JSON.parse(filter);
+            let selectedCities = filter.filter(element => element.type == 'city').map(element => element.name);;
+            let selectedDistricts = filter.filter(element => element.type == 'district').map(element => element.name);
+            console.log(selectedCities);
+            console.log(selectedDistricts);
+            if (selectedCities.length > 0 && selectedDistricts.length > 0) {
+                employees = await employeesModel.find({
+                    city: { $in: selectedCities },
+                    district: { $in: selectedDistricts }
+                });
+            } else if (selectedCities.length > 0) {
+                employees = await employeesModel.find({
+                    city: { $in: selectedCities },
+                });
+            } else if (selectedDistricts.length > 0) {
+                employees = await employeesModel.find({
+                    district: { $in: selectedDistricts }
+                });
+            } else {
+                employees = await employeesModel.find({});
+            }
+        } else {
+            employees = await employeesModel.find({});
+        }
         res.send(employees);
     } catch (error) {
         console.log(error);
@@ -43,7 +69,7 @@ router.delete('/employees/:id', async (req, res, next) => {
 
 router.put('/employees', async (req, res, next) => {
     try {
-        let status = await employeesModel.updateOne( {_id: req.body._id}, req.body );
+        let status = await employeesModel.updateOne({ _id: req.body._id }, req.body);
         res.send(status);
     } catch (error) {
         console.log(error);
